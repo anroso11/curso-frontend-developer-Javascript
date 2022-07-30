598386f2d26f23a4f26f7f743f5e33274d28200b
anroso11 <andresroberto1187@gmail.com>
2022-07-30 18:40:58 -0500

    Se agrego el menu desplegable para moviles

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
 const navBarEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.inactive');
+const desktopMenu = document.querySelector('.desktop-menu');
+const iconMenu = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
-navBarEmail.addEventListener('click', showMenu);
+navBarEmail.addEventListener('click', showMenuDesktop);
+iconMenu.addEventListener('click', showMenuMobile);
 
-function showMenu () {
+function showMenuDesktop () {
     desktopMenu.classList.toggle('inactive');
 }
+
+function showMenuMobile () {
+    mobileMenu.classList.toggle('inactive');
+}
+
