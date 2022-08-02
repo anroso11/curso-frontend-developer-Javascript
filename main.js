3f0b6005c30f56e5652a475c1fcf948d12b9fc32
anroso11 <andresroberto1187@gmail.com>
2022-08-02 12:29:00 -0500

    Se agrego componente visualización del producto

--- main.js
+++ main.js
@@ -3,13 +3,19 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const iconMenu = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const carritoMenuCompra = document.querySelector('.product-detail');
+const productDetailContainer = document.querySelector('.product-detail-square');
+const productDetailCloseIcon = document.querySelector('.product-detail-close')
 const iconCarrito = document.querySelector('.navbar-shopping-cart');
 const cardsContainer = document.querySelector('.cards-container');
+const productDetailImage = document.querySelector('.product-detail-square > img');
+const productDetailPrice = document.querySelector('#product-price');
+const productDetailName = document.querySelector('#product-name');
 
 
 navBarEmail.addEventListener('click', showMenuDesktop);
 iconMenu.addEventListener('click', showMenuMobile);
 iconCarrito.addEventListener('click', showMenuCarrito);
+productDetailCloseIcon.addEventListener('click', closeProductDetailAside);
 
 function showMenuDesktop () {
 
@@ -36,10 +42,14 @@ function showMenuMobile () {
     if(!iscarritoMenuClosed){
         carritoMenuCompra.classList.add('inactive');
     }
+
+    closeProductDetailAside();
     mobileMenu.classList.toggle('inactive');
 }
 
 function showMenuCarrito() {
+
+  
 
      /*Este Funciona agregar add*/    
     // carritoMenuCompra.classList.toggle('inactive');
@@ -50,8 +60,37 @@ function showMenuCarrito() {
     if(!ismobileMenuClosed){
         mobileMenu.classList.add('inactive');
     }
+   
+    const isProductDetailClosed = productDetailContainer.classList.contains('inactive');
+
+    if(!isProductDetailClosed){
+        productDetailContainer.classList.add('inactive');
+    }
+
     carritoMenuCompra.classList.toggle('inactive');
   
+}
+
+function openProductDetailAside(event) {
+
+    carritoMenuCompra.classList.add('inactive');
+    productDetailContainer.classList.remove('inactive');
+    setProductInfo(event);
+}
+
+function setProductInfo(data) {
+    const product = data.path.find(first => first.className === 'product-card');
+    const productImage = product.querySelector('img');
+    const productName = product.querySelector('.product-info div p:nth-child(2)'); 
+    const productPrice = product.querySelector('.product-info div p:nth-child(1)');
+    productDetailImage.setAttribute('src', productImage.getAttribute('src'));
+    productDetailPrice.innerText = productPrice.innerText;
+    productDetailName.innerText = productName.innerText;
+
+}
+
+function closeProductDetailAside() {
+    productDetailContainer.classList.add('inactive');
 }
 
 const productList = [];
@@ -76,6 +115,46 @@ productList.push({
     price: 350,
     image: 'https://exitocol.vtexassets.com/arquivos/ids/10044382-1200-auto?v=637674354306070000&width=1200&height=auto&aspect=true',
 })
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1200,
+    image: 'https://assets.specialized.com/i/specialized/60821-104_HLMT_ALIGN-II-HLMT-MIPS-CE-BLK-BLKREFL-S-M_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1600,
+    image: 'https://m.media-amazon.com/images/I/61eExL-rIAL._AC_SL1001_.jpg'
+});
+productList.push ({
+    name:'Bicycle helmet',
+    price: 1500,
+    image: 'https://assets.specialized.com/i/specialized/60822-140_HLMT_CHAMONIX-HLMT-MIPS-CE-MRN-M-L_HERO?bg=rgb(241,241,241)&w=1600&h=900&fmt=auto'
+});
+productList.push ({
+    name:'Seat',
+    price: 300,
+    image: 'https://m.media-amazon.com/images/I/61e+sZ9rgNL._AC_SL1500_.jpg'
+});
+productList.push ({
+    name:'Tennis Montain Bike',
+    price: 2200,
+    image: 'https://assets.adidas.com/images/h_840,f_auto,q_auto,fl_lossy,c_fill,g_auto/8ea578f6c07847fca2d0ac85011d7f1f_9366/Tenis_para_Mountain_Bike_Five_Ten_Freerider_Negro_FW2835_01_standard.jpg'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 800,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/gafas-siroko-tech-k3s-london-lateral/1200x/crop_center.jpg?v=1635209602'
+});
+productList.push ({
+    name:'Sunglasses',
+    price: 600,
+    image: 'https://cdn.siroko.com/s/files/1/1220/6874/products/siroko-tech-k3s-clearfog-lente-antiniebla-frontal/1200x/crop_center.jpg?v=1635209603'
+});
+productList.push ({
+    name:'Bicycle seat bag',
+    price: 876,
+    image: 'https://m.media-amazon.com/images/I/81k2Gmal+VL._AC_SL1500_.jpg'
+}); 
 
 /*<div class="product-card">
 <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
@@ -99,6 +178,7 @@ function renderProducts (arr){
 
         const productImg = document.createElement('img');
         productImg.setAttribute('src', product.image);
+        productImg.addEventListener('click', openProductDetailAside);
 
         const productInfo = document.createElement('div');
         productInfo.classList.add('product-info');
